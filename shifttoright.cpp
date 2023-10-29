8eef7c040d02cb40376f40e19504d4aaebe9d265
FidanAmiraslan <147319209+FidanAmiraslan@users.noreply.github.com>
2023-10-29 13:31:25 +0400

    .

---
+++ shifttoright.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+void get_arr(int arr[],int size);
+void print_arr(int arr[], int size);
+
+int main()
+{
+	int size,shift;
+	printf("Enter size of array = ");
+	scanf("%d",&size);
+	
+	int arr[size];
+	get_arr(arr,size);
+	print_arr(arr,size);
+	printf("\n");
+	                                      
+	printf("Shift = ");                       
+	scanf("%d",&shift);	
+	
+    
+	for (int m = 0; m < shift; m++) 
+    {
+		for (int i = size -1 ; i > 0; i--)   
+		{                              
+			int temp = arr[i];
+			arr[i] = arr[i - 1];
+			arr[i - 1] = temp;
+		
+		}	
+	}
+	
+	printf("Array after shifting right by %d positions:",shift);
+	print_arr(arr,size);
+
+
+}
+
+
+void get_arr(int arr[],int size)
+{
+	for(int i = 0; i < size; i++)
+	{
+		printf("Element of array = ");
+		scanf("%d",&arr[i]);
+	}
+}
+
+
+void print_arr(int arr[], int size)
+{
+	for(int i = 0; i < size; i++)
+	{
+		printf("%d ",arr[i]);	
+	}
+}
+
+
